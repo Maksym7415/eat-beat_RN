20289b248af49d50a52075e2fd95351c201604b3
telesensdev <62094026+telesensdev@users.noreply.github.com>
2020-11-12 15:01:49 +0200

    fix history, fix daily norm loader

--- screens/Home/HistoryScreen.tsx
+++ screens/Home/HistoryScreen.tsx
@@ -34,6 +34,7 @@ const HistoryScreen: FC<NavProps> = ({ navigation }) => {
   const [offset, setOffset] = useState<Offset>({ count: 0, offset: 0 });
 
   const getHealthsScore = async () => {
+    console.log(124)
     const response = await server.getHistory(offset.offset);
     if (response.ok) {
       const historyFeed: HealthScore[] = response.data.data;
@@ -56,8 +57,10 @@ const HistoryScreen: FC<NavProps> = ({ navigation }) => {
   };
 
   useEffect(() => {
-    getHealthsScore();
-  }, [refresh]);
+    navigation.addListener('focus', () => {
+      getHealthsScore();
+    });
+  }, []);
 
   return (
     <View style={styles.canvas}>
