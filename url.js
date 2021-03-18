e14729ae119cc0e39b0c053bc892dfffaab6c502
Victor Nadushkin <victor.nadushkin@factorialcomplexity.com>
2021-03-18 16:48:44 +0200

    Remove url.js

--- url.js
+++
@@ -1,8 +0,0 @@
-const localURL = "http://10.4.30.157:8081/";
-const devLocalURL = "http://192.168.3.115:8081/";
-const productionURL = "https://logisticbrocker.hopto.org/eat-beat/";
-const temporaryProductionUrl = "http://52.72.42.64:8081/";
-const QA = "https://logisticbrocker.hopto.org/eat-beat-test/";
-const testURL = "https://logisticbrocker.hopto.org/eat-beat-test/";
-// export const baseURL = QA;
-export const baseURL = devLocalURL;
